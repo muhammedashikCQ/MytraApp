4408db0669f3953b3075dd019984a64aef1e094f
AishaRahim <aisha.rahim@controlqore.com>
2023-09-11 14:13:38 +0530

    Services Api Created

---
+++ BackEnd/MytraAppApi/MytraApp/Request/Query/Services/GetServices/GetServicesQuery.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using Models = MytraModel.Models;
+
+using Microsoft.EntityFrameworkCore.Infrastructure;
+
+namespace MytraApp.Request.Query.Services.GetServices
+{
+    public class GetServicesQuery:IRequest<List<Models.Service>>
+    {
+    }
+}
---
+++ BackEnd/MytraAppApi/MytraModel/Enums/LocationEnum.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MytraModel.Enums
+{
+    public enum LocationEnum
+    {
+        Trivandrum =1,
+        Kollam,
+        Kochi,
+        Kottayam,
+        Idukki
+
+    }
+}
