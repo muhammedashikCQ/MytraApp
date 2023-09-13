b76126066ab0f1f5ac5c05ba04b952e0dab236ce
muhammedashikCQ <muhammed.ashik@controlqore.com>
2023-09-13 15:06:35 +0530

    RequestPageAdded

--- FrontEnd/mytra_app/lib/views/splash_screen.dart
+++ FrontEnd/mytra_app/lib/views/splash_screen.dart
@@ -14,17 +14,6 @@ class SplashPage extends StatefulWidget {
 class _SplashPageState extends State<SplashPage> {
   static Box<String> idBox = Hive.box<String>("idBox");
 
-  // @override
-  // void initState() {
-  //   super.initState();
-  //   // Check if the Hive box is empty
-  //   if (idBox.isEmpty) {
-  //     navigateToWelcome();
-  //   } else {
-  //     navigateToHome();
-  //   }
-  // }
-
   @override
   void initState() {
     super.initState();
@@ -34,16 +23,11 @@ class _SplashPageState extends State<SplashPage> {
   initHiveAndNavigate() async {
     await Hive.openBox<String>("idBox");
 
-    // Check if the user ID is stored in Hive
     String? userId = idBox.get("userId");
 
-    print("User ID from Hive: $userId"); // Add this line for debugging
-
     if (userId != null) {
-      // User ID exists, navigate to home page
       navigateToHome();
     } else {
-      // User ID doesn't exist, navigate to welcome page
       navigateToWelcome();
     }
   }
